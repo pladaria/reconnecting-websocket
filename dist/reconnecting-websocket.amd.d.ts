8de0241ee02d63b5da9cc656ce9acccd5cb42639
Pedro Ladaria <pedro.ladaria@gmail.com>
2017-05-20 14:47:01 +0200

    generate typings

---
+++ dist/reconnecting-websocket.amd.d.ts
@@ -0,0 +1,12 @@
+declare module "index" {
+    const ReconnectingWebsocket: (url: string, protocols?: string | string[], options?: {
+        constructor?: new (url: string, protocols?: string | string[]) => WebSocket;
+        maxReconnectionDelay?: number;
+        minReconnectionDelay?: number;
+        reconnectionDelayGrowFactor?: number;
+        connectionTimeout?: number;
+        maxRetries?: number;
+        debug?: boolean;
+    }) => void;
+    export = ReconnectingWebsocket;
+}
